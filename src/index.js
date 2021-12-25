799cc59d58da710de7e9ccaad320df611f6f9e97
Myika Marshall <myika.marshall@gmail.com>
2021-12-24 23:04:11 -0600

    Updated components to folders vs files

--- src/index.js
+++ src/index.js
@@ -9,11 +9,19 @@ import logger from 'redux-logger';
 
 const feedbackReducer = (state = {}, action) => {
     switch(action.type){
-        case 'SET_HOME':
-            return{...state, home: action.payload};
-        case 'SET_FEELING':
-            return{...state, feeling: action.payload};
-    }};
+    case 'SET_HOME':
+        return{...state, home: action.payload};
+    case 'SET_FEELING':
+        return{...state, feeling: action.payload};
+        case 'SET_UNDERSTANDING':
+        return{...state, understanding: action.payload};
+        case 'SET_SUPPORT':
+        return{...state, support: action.payload};
+        case 'SET_COMMENT':
+        return{...state, comment: action.payload};
+        default: return state;
+    }
+};
 
 
     const storeInstance = createStore(
